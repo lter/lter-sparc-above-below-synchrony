ca4ea0ac854b96cde0e68b2600b526c0bd6054e4
njlyon0 <nicholasjlyon@gmail.com>
2025-11-05 11:35:07 -0500

    feat (neon): removing superseded/deprecated content from download script

--- neon-01_download.r
+++ neon-01_download.r
@@ -18,9 +18,6 @@ rm(list = ls()); gc()
 purrr::walk(.x = dir(path = file.path("tools"), pattern = "fxn_"),
             .f = ~ source(file.path("tools", .x)))
 
-# Grab current year
-this_year <- as.numeric(stringr::str_sub(Sys.Date(), start = 1, end = 4))
-
 ## -------------------------------------- ##
 # Download Plant Presence/Percent Cover Data ----
 ## -------------------------------------- ##
@@ -28,13 +25,11 @@ this_year <- as.numeric(stringr::str_sub(Sys.Date(), start = 1, end = 4))
 # Download plant precence & percent cover data for all years / sites
 download_neon(dpi = "DP1.10058.001", 
               ## Start year identified from NEON catalog
-              start_yr = 2013, end_yr = this_year,
+              ## End year is final year of non-provisional data
+              start_yr = 2013, end_yr = 2023,
               prefix = "plant-presence-percent-cover_", 
               dest = file.path("data", "raw_neon"),
               wanted_files = c("div_1m2Data", "div_10m2Data100m2Data", "variables_10058"))
-
-# Clear environment
-rm(list = ls()); gc()
 
 ## -------------------------------------- ##
 # Download Soil Microbe Taxonomy Data ----
@@ -43,12 +38,9 @@ rm(list = ls()); gc()
 # Downlaod soil microbe community taxonomy data for all years / sites
 download_neon(dpi = "DP1.10081.002", 
               ## Start year identified from NEON catalog
-              start_yr = 2016, end_yr = this_year,
+              start_yr = 2016, end_yr = 2025,
               prefix = "soil-microbe-comm-taxonomy_", 
               dest = file.path("data", "raw_neon"),
               wanted_files = c("div_1m2Data", "div_10m2Data100m2Data", "variables_10058"))
-
-# Clear environment
-rm(list = ls()); gc()
 
 # End ----
