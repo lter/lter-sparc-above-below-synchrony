7bac16866d035bd358c6839fe7f27905e16f3298
njlyon0 <nicholasjlyon@gmail.com>
2025-11-05 09:55:03 -0500

    feat (neon): skeleton for new download script

---
+++ neon-01_download.r
@@ -0,0 +1,19 @@
+## ------------------------------------------------------- ##
+# NEON Data - Download Above/Below Files
+## ------------------------------------------------------- ##
+
+# Load libraries
+librarian::shelf(tidyverse)
+
+# Do setup tasks
+source(file = file.path("00_setup.r"))
+
+# Clear environment
+rm(list = ls()); gc()
+
+## -------------------------------------- ##
+
+## -------------------------------------- ##
+
+
+# End ----
