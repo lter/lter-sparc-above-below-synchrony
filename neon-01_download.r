b5e057a4a94582ab72d260083c804a57e1bdd645
njlyon0 <nicholasjlyon@gmail.com>
2025-11-05 10:39:38 -0500

    feat (neon): complete draft of data download script for plants and microbes

--- neon-01_download.r
+++ neon-01_download.r
@@ -23,9 +23,13 @@ this_month <- stringr::str_sub(Sys.Date(), start = 1, end = 7)
 ## -------------------------------------- ##
 
 # Define some useful objects for making the loop more flexible
+## NEON DPI code for data to download
 focal_dpi <- "DP1.10058.001"
+## Filename prefix for exported data (should be unique to this DPI but more informative)
+focal_prefix <- "plant-presence-percent-cover_"
 
 # Loop across potentially-relevant years
+## First year identified from NEON catalog
 for(focal_yr in 2013:this_year){
   # focal_yr <- 2013
 
@@ -66,31 +70,72 @@ for(focal_yr in 2013:this_year){
       # Actually export files (folders created in `00_setup.r`)
       write.csv(x = focal_list[[wanted_file]], na = '', row.names = F,
                 file = file.path("data", "raw_neon", 
-                                paste0("plant_", wanted_file, "_", focal_yr, ".csv")))
+                                paste0(focal_prefix, wanted_file, "_", focal_yr, ".csv")))
 
     } # Close export loop
   } # Close download conditional
 } # Close year loop
 
-
-
-# Identify relevant plant data
-## Note that this operation is quite slow (several mins)
-# plants <- neonUtilities::loadByProduct(
- #  dpID = "DP1.10058.001", 
-  # startdate = "2018-01", 
-  # enddate = stringr::str_sub(Sys.Date(), start = 1, end = 7), 
-  # check.size = F)
-
-
-
+# Clear environment
+rm(list = ls()); gc()
 
 ## -------------------------------------- ##
 
 ## -------------------------------------- ##
 
 # Identify desired NEON DPI codes
-dpi_micro <- "DP1.10081.002"
+focal_dpi <- "DP1.10081.002"
+focal_prefix <- "soil-microbe-comm-taxonomy_"
 
+# Loop across potentially-relevant years
+## First year identified from NEON catalog
+for(focal_yr in 2016:this_year){
+  # focal_yr <- 2016
+
+  # Identify any data for the focal year that we already have
+  local_data <- dir(path = file.path("data", "raw_neon"), 
+                    pattern = paste0("_", focal_yr, ".csv"))
+
+  # Skip this year if we already have the expected number of files
+  ## Note that the expected number of local files is hard-coded
+  if(length(local_data) == 3){ 
+    
+    # Completion message
+    message("Data for ", focal_yr, " already downloaded")
+
+  # Otherwise, download the data!
+  } else {
+    
+    # Progress message
+    message("Acquiring data for ", focal_yr)
+
+    # Grab start/end months for the focal year
+    focal_start <- paste0(focal_yr, "-01")
+    if(focal_yr != this_year){
+      focal_end <- paste0(focal_yr, "-12")
+    } else { focal_end <- this_month }
+
+    # Identify relevant plant data
+    focal_list <- neonUtilities::loadByProduct(
+      dpID = focal_dpi, 
+      startdate = focal_start, 
+      enddate = focal_end, 
+      check.size = F)  
+
+    # Export relevant stuff
+    for(wanted_file in c("div_1m2Data", "div_10m2Data100m2Data", "variables_10058")){
+      # wanted_file <- "div_1m2Data"
+
+      # Actually export files (folders created in `00_setup.r`)
+      write.csv(x = focal_list[[wanted_file]], na = '', row.names = F,
+                file = file.path("data", "raw_neon", 
+                                paste0(focal_prefix, wanted_file, "_", focal_yr, ".csv")))
+
+    } # Close export loop
+  } # Close download conditional
+} # Close year loop
+
+# Clear environment
+rm(list = ls()); gc()
 
 # End ----
