cb31f787ab4a806b3cc49ee1c21f2507f1f7f0ed
L. McKinley Nevins <lmnevins23@gmail.com>
2025-11-10 15:37:09 -0800

    chore: remove unneeded data types

--- data_download_NEON.R
+++ data_download_NEON.R
@@ -28,11 +28,10 @@ library(ecocomDP); packageVersion("ecocomDP")
 
 # load in NEON token 
 
-wd <- "~/Dropbox/WSU/SSECR/ssecr-above-belowground-coupling/" # this will depend on your local machine
+wd <- "~/Dropbox/WSU/LTER_SPARC/NEON/"
 setwd(wd)
 
 source(paste0(wd, "/neon_token_source.R"))
-
 
 
 ###############
@@ -46,15 +45,6 @@ source(paste0(wd, "/neon_token_source.R"))
 # Not great temporal coverage though, in general all sampling was 2016-2018/19
   # Soil community taxonomy: DP1.10081.002 (SUCCESS) 
       # Soil community composition is the previous version of this that was depricated 
-  # Soil microbe group abundances: DP1.10109.001 (SUCCESS)
-  # Soil microbe marker gene sequences: DP1.10108.001 (SUCCESS)
-  # Soil microbe metagenome sequences: DP1.10107.001 (SUCCESS)
-
-# Biomass datasets: 
-  # Root biomass and chemistry, periodic: DP1.10067.001 (SUCCESS)
-        # Not doing the megapit dataset because this was collected just once at site establishment 
-  # Soil microbe biomass: DP1.10104.001  (SUCCESS)
-
 
 # ***** For all of these, the date of the file save to the project folder can indicate the date accessed for the citation.
 
@@ -119,6 +109,38 @@ saveRDS(plants,"~/Dropbox/WSU/LTER_SPARC/NEON/NEON_plant_data.rds")
 
 #########################################################################################################
 
+# Management and events reporting 
+
+manage <- neonUtilities::loadByProduct(
+  dpID = "DP1.10111.001", # the NEON management and event reporting product 
+  site = c("BONA", "CLBJ", "CPER", "GUAN", "HARV", "KONZ", "NIWO", "ONAQ",
+           "ORNL", "OSBS", "PUUM", "SCBI", "SJER", "SRER", "TALL", "TOOL",
+           "UNDE", "WOOD", "WREF", "YELL"), # doing just core sites that have compatible data with belowground 
+  startdate = "2016-01", # start year-month - no compatible belowground data earlier than this 
+  enddate = "2024-12", # end year-month - data release is through December 2024
+  token = Sys.getenv("NEON_TOKEN"), # use NEON_TOKEN environmental variable
+  check.size = F) # proceed with download regardless of file size
+
+
+# Inspect 
+names(manage)
+
+# extract items from list and put in R env. 
+manage %>% list2env(.GlobalEnv)
+
+
+# actual event data 
+write.csv(sim_eventData,"~/Dropbox/WSU/LTER_SPARC/NEON/site_events_NEON.csv", row.names = FALSE)
+
+# categorical codes
+write.csv(categoricalCodes_10111,"~/Dropbox/WSU/LTER_SPARC/NEON/site_event_codes_NEON.csv", row.names = FALSE)
+
+# list of variables 
+write.csv(variables_10111,"~/Dropbox/WSU/LTER_SPARC/NEON/site_events_NEON_meta.csv", row.names = FALSE)
+
+
+#########################################################################################################
+
 #####################
 ## Exploratory, Geoff is doing the raw sequences 
 #####################
@@ -169,222 +191,6 @@ write.csv(variables_10081,"~/Dropbox/WSU/LTER_SPARC/NEON/NEON_B_COMP_meta.csv", 
 
 # save the whole stack of files as an R object so I could load it back in later if necessary 
 saveRDS(microbes_com,"~/Dropbox/WSU/LTER_SPARC/NEON/NEON_microbe_comp_data_2024.rds")
-
-
-#########################################################################################################
-
-
-######################
-# STOP
-
-# !! These are available if desired, but haven't been downloaded yet !! 
-# Could focus sites and date range depending on core plant/microbe data choices before downloading, 
-# to make these easier to manage 
-######################
-
-
-
-# DOWNLOAD
-
-# Download the soil microbial group abundance data 
-
-microbes_group <- neonUtilities::loadByProduct(
-  dpID = "DP1.10109.001", # the NEON soil microbial group abundance data product 
-  # site = c(""), # ignoring site because I want all sites that have data available
-  startdate = "2012-12", # start year-month - just before the first microbial sampling was started in 2014
-  enddate = "2019-12", # end year-month - through the end of the time that the soil data was collected 
-  token = Sys.getenv("NEON_TOKEN"), # use NEON_TOKEN environmental variable
-  check.size = F) # proceed with download regardless of file size
-
-# Inspect 
-names(microbes_group)
-
-# extract items from list and put in R env. 
-microbes_group %>% list2env(.GlobalEnv)
-
-# data file of interest:
-# mga_soilGroupAbundances - Laboratory results of gene copy number data in soil samples
-# variables_10109 - Defines all of the columns in the data files 
-
-# save some data files for reference 
-
-# following team naming conventions for the data file 
-
-# group abundance data 
-write.csv(mga_soilGroupAbundances,"~/Dropbox/WSU/LTER_SPARC/NEON/B_COM_NEON_001.csv", row.names = FALSE)
-
-# list of variables 
-write.csv(variables_10109,"~/Dropbox/WSU/LTER_SPARC/NEON/B_COM_NEON_001.2_meta.csv", row.names = FALSE)
-
-# save the whole stack of files as an R object so I could load it back in later if necessary 
-saveRDS(microbes_group,"~/Dropbox/WSU/LTER_SPARC/NEON/NEON_microbe_group_data_2024.rds")
-
-
-#########################################################################################################
-
-# DOWNLOAD
-
-# Download the soil microbial marker gene sequence data 
-
-marker_group <- neonUtilities::loadByProduct(
-  dpID = "DP1.10108.001", # the NEON soil microbial marker gene sequence data 
-  # site = c(""), # ignoring site because I want all sites that have data available
-  package = "expanded", 
-  startdate = "2012-12", # start year-month - just before the first microbial sampling was started in 2014
-  enddate = "2024-12", # end year-month - marker gene sampling still seems to be ongoing, but provisional data is only through 2019
-  token = Sys.getenv("NEON_TOKEN"), # use NEON_TOKEN environmental variable
-  check.size = F) # proceed with download regardless of file size
-
-# Inspect 
-names(marker_group)
-
-# extract items from list and put in R env. 
-marker_group %>% list2env(.GlobalEnv)
-
-# data file of interest:
-# mmmg_soilRawDataFiles - Raw sequence data files for microbial marker gene sequencing from soil samples
-# variables_10108 - Defines all of the columns in the data files 
-
-# Defining the package as 'expanded' solved the problems of the data files being absent 
-
-
-# marker gene raw data files
-write.csv(mmg_soilRawDataFiles,"~/Dropbox/WSU/LTER_SPARC/NEON/NEON_marker_gene_raw_data.csv", row.names = FALSE)
-
-# list of variables 
-write.csv(variables_10108,"~/Dropbox/WSU/LTER_SPARC/NEON/NEON_B_marker_gene_meta.csv", row.names = FALSE)
-
-# save the whole stack of files as an R object so I could load it back in later if necessary 
-saveRDS(marker_group,"~/Dropbox/WSU/LTER_SPARC/NEON/NEON_marker_gene_data_2024.rds")
-
-
-#########################################################################################################
-
-# DOWNLOAD
-
-# Download the soil microbial metagenome sequence data 
-
-microbes_metagen <- neonUtilities::loadByProduct(
-  dpID = "DP1.10107.001", # the NEON soil microbial metagenome sequence data product 
-  # site = c(""), # ignoring site because I want all sites that have data available
-  package = "expanded", 
-  startdate = "2012-12", # start year-month - just before the first microbial sampling was started in 2014
-  enddate = "2024-12", # end year-month - through the end of the time that the soil data was collected 
-  token = Sys.getenv("NEON_TOKEN"), # use NEON_TOKEN environmental variable
-  check.size = F) # proceed with download regardless of file size
-
-# Inspect 
-names(microbes_metagen)
-
-# extract items from list and put in R env. 
-microbes_metagen %>% list2env(.GlobalEnv)
-
-# data file of interest:
-# mms_rawDataFiles - Soil metagenomics raw sequence data
-# variables_10107 - Defines all of the columns in the data files 
-
-# save some data files for reference 
-
-# following team naming conventions for the data file 
-
-# metagenome data 
-write.csv(mms_rawDataFiles,"~/Dropbox/WSU/LTER_SPARC/NEON/NEON_metagenome_raw_data.csv", row.names = FALSE)
-
-# list of variables 
-write.csv(variables_10107,"~/Dropbox/WSU/LTER_SPARC/NEON/NEON_B_metagenome_meta.csv", row.names = FALSE)
-
-# save the whole stack of files as an R object so I could load it back in later if necessary 
-saveRDS(microbes_metagen,"~/Dropbox/WSU/LTER_SPARC/NEON/NEON_metagenome_data_2024.rds")
-
-#########################################################################################################
-
-# DOWNLOAD
-
-# Download the Root biomass and chemistry, periodic data
-
-root_biomass <- neonUtilities::loadByProduct(
-  dpID = "DP1.10067.001", # the NEON Root biomass and chemistry, periodic data product 
-  # site = c(""), # ignoring site because I want all sites that have data available
-  package = "expanded", 
-  startdate = "2015-12", # start year-month - started in 2016, sites sampled every 5 years 
-  enddate = "2024-12", # end year-month - through the end of the time that the soil data was collected 
-  token = Sys.getenv("NEON_TOKEN"), # use NEON_TOKEN environmental variable
-  check.size = F) # proceed with download regardless of file size
-
-# Inspect 
-names(root_biomass)
-
-# extract items from list and put in R env. 
-root_biomass %>% list2env(.GlobalEnv)
-
-# data file of interest:
-# bbc_rootChemistry - C and N and stable isotopes for all root samples 
-# bbc_rootmass - Root dry mass, also includes size classes and if mycorrhizae were visible, etc. 
-# bbc_percore - Info on root volume per sampled core of soil 
-# variables_10067 - Defines all of the columns in the data files 
-
-# save some data files for reference 
-# root chemistry data 
-write.csv(bbc_rootChemistry,"~/Dropbox/WSU/LTER_SPARC/NEON/NEON_rootchem_raw_data.csv", row.names = FALSE)
-
-# root mass data 
-write.csv(bbc_rootmass,"~/Dropbox/WSU/LTER_SPARC/NEON/NEON_rootmass_raw_data.csv", row.names = FALSE)
-
-# root volume per core data 
-write.csv(bbc_percore,"~/Dropbox/WSU/LTER_SPARC/NEON/NEON_rootvolume_raw_data.csv", row.names = FALSE)
-
-# list of variables 
-write.csv(variables_10067,"~/Dropbox/WSU/LTER_SPARC/NEON/NEON_B_rootbiomass_meta.csv", row.names = FALSE)
-
-# save the whole stack of files as an R object so I could load it back in later if necessary 
-saveRDS(root_biomass,"~/Dropbox/WSU/LTER_SPARC/NEON/NEON_rootbiomass_data_2024.rds")
-
-# Saved the first three, these could be combined into one file, probably, so this important info is more condensed 
-
-#########################################################################################################
-
-# DOWNLOAD
-
-# Download the Soil microbe biomass data
-
-microbe_biomass <- neonUtilities::loadByProduct(
-  dpID = "DP1.10104.001", # the NEON Soil microbe biomass data product 
-  # site = c(""), # ignoring site because I want all sites that have data available
-  package = "expanded", 
-  startdate = "2016-12", # start year-month - started March 2017, sampling happens 1-3 times per year 
-  enddate = "2024-12", # end year-month - ongoing sampling, ~240 day lag time 
-  token = Sys.getenv("NEON_TOKEN"), # use NEON_TOKEN environmental variable
-  check.size = F) # proceed with download regardless of file size
-
-# Inspect 
-names(microbe_biomass)
-
-# extract items from list and put in R env. 
-microbe_biomass %>% list2env(.GlobalEnv)
-
-# data file of interest:
-# sme_microbialBiomass - lab results of microbial biomass data  
-# sme_scaledMicrobialBiomass - Laboratory results of microbial biomass data, scaled to an internal standard
-# sme_batchResults - Microbial biomass batch-level data, summarizes a total lipid volume combined 
-# variables_10104 - Defines all of the columns in the data files 
-
-# save some data files for reference 
-# microbial biomass data 
-write.csv(sme_microbialBiomass,"~/Dropbox/WSU/LTER_SPARC/NEON/NEON_microbe_biomass_raw_data.csv", row.names = FALSE)
-
-# microbial biomass scaled data  
-write.csv(sme_scaledMicrobialBiomass,"~/Dropbox/WSU/LTER_SPARC/NEON/NEON_microbe_biomass_scaled_data.csv", row.names = FALSE)
-
-# microbial biomass batch-level data  
-write.csv(sme_batchResults,"~/Dropbox/WSU/LTER_SPARC/NEON/NEON_microbe_biomass_batch_data.csv", row.names = FALSE)
-
-# list of variables 
-write.csv(variables_10104,"~/Dropbox/WSU/LTER_SPARC/NEON/NEON_B_microbebiomass_meta.csv", row.names = FALSE)
-
-# save the whole stack of files as an R object so I could load it back in later if necessary 
-saveRDS(microbe_biomass,"~/Dropbox/WSU/LTER_SPARC/NEON/NEON_microbebiomass_data_2024.rds")
-
-# Saved the first three, these could be combined into one file, probably, so this important info is more condensed 
 
 
 #########################################################################################################
