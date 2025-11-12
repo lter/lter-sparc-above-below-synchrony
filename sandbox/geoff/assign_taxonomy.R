7f0a5b4e43ddef7847a8f2a3b1ecbe75d9909666
gzahn <geoff.gone@gmail.com>
2025-11-12 15:54:05 -0500

    add taxonomic assignment script

---
+++ sandbox/geoff/assign_taxonomy.R
@@ -0,0 +1,65 @@
+
+
+# SETUP ####
+
+## packages ####
+library(tidyverse)
+library(dada2)
+library(phyloseq)
+
+fung_db <- "/sciclone/scr10/gzahn/DATABASES/Eukaryome/Eukaryome_General_SSU_v1.8_reformatted.fasta.gz"
+bact_db <- "/sciclone/scr10/gzahn/DATABASES/Silva/silva_nr99_v138.2_toSpecies_trainset.fa.gz"
+bact_spp_db <- "/sciclone/scr10/gzahn/DATABASES/Silva/silva_v138.2_assignSpecies.fa.gz"
+
+## Load physeq objects and extract reads
+
+bact <- readRDS("./output/bacterial_physeq_no_taxonomy.RDS") 
+bact_reads <- bact %>% otu_table() %>% colnames()
+fung <- readRDS("./output/fungal_physeq_no_taxonomy.RDS")
+fung_reads <- fung %>% otu_table() %>% colnames()
+
+
+# ASSIGN TAXONOMY ####
+
+## bacteria ####
+bact_tax <- 
+  assignTaxonomy(bact_reads,
+                 refFasta = bact_db,
+                 multithread = TRUE,
+                 tryRC = TRUE,
+                 minBoot = 50)
+
+# run addSpecies algorithm and add to previous tax table
+bact_tax_spp <- addSpecies(taxtab = bact_tax,refFasta = bact_spp_db,tryRC = TRUE)
+# save progress
+saveRDS(bact_tax_spp,"./output/bact_tax_table.RDS")
+
+
+## fungi ####
+fung_tax <- 
+  assignTaxonomy(fung_reads,
+                 refFasta = euk_db,
+                 multithread = TRUE,
+                 tryRC = TRUE,
+                 minBoot = 50)
+# save progress
+saveRDS(fung_tax,"./output/fung_tax_table.RDS")
+
+
+# ADD TO PHYSEQ ####
+bact_ps <- phyloseq(bact, tax_table(bact_tax))
+fung_ps <- phyloseq(fung, tax_table(fung_tax))
+
+# remove non-fungi/non-bacteria
+bact_ps <- 
+  bact_ps %>% 
+  subset_taxa(Kingdom == "Bacteria" & Family != "Mitochondria" & Order != "Chloroplast")
+
+fung_ps <- 
+  fung_ps %>% 
+  subset_taxa(Kingdom == "Fungi" & !is.na(Phylum))
+
+# EXPORT ####
+saveRDS(bact_ps, "./output/bacterial_physeq.RDS")
+saveRDS(fung_ps, "./output/fungal_physeq.RDS")
+
--- sandbox/geoff/combine_asv_tables.R
+++ sandbox/geoff/combine_asv_tables.R
@@ -21,8 +21,10 @@ n_dupl <- function(x){
 }
 
 ## Options ####
-setwd("~/Desktop/GIT_REPOSITORIES/lter-sparc-above-below-synchrony/sandbox/geoff/")
 # LOAD DATA ####
+
+## Get metadata ####
+meta <- readRDS("./full_microbiome_dataset_info.RDS")
 
 ## Find ASV tables ####
 asv_fps <- list.files("./output",full.names = TRUE, recursive = TRUE)
@@ -49,16 +51,67 @@ gc()
 
 # combine bacteria
 bact <- bact_asvs %>% reduce(merge_phyloseq)
+
+# identify singleton taxa/samples and remove
+bact_good_samples <- rowSums(bact) > 1
+bact_good_taxa <- colSums(bact) > 1
+bact <- bact[bact_good_samples,bact_good_taxa]
+
+# indentify metadata rows for bacteria
+bact_meta <- 
+  meta %>% 
+  dplyr::filter(rawDataFileName %in% row.names(bact)) %>% 
+  dplyr::select(plotID,year,m_y,dnaSampleID,
+                siteID,collectDate,forwardPrimer,
+                reversePrimer,sequencerRunID,
+                rawDataFileName)
+
+# build metadata for physeq
+bact_samdata <- sample_data(bact_meta)
+sample_names(bact_samdata) <- bact_samdata$rawDataFileName
+
+# build physeq object
+bact_ps <- phyloseq(bact_samdata,bact)
+bact_ps@sam_data$rawDataFileName <- NULL
+
 # save to output
+saveRDS(bact_ps,"./output/bacterial_physeq_no_taxonomy.RDS")
 saveRDS(bact,"./output/bacterial_asv_table_combined.RDS")
+
 # clean up
 rm(list=c("bact","bact_asvs"))
 gc()
 
 # combine fungi
 fung <- fung_asvs %>% reduce(merge_phyloseq)
+
+# identify singleton taxa/samples and remove
+fung_good_samples <- rowSums(fung) > 1
+fung_good_taxa <- colSums(fung) > 1
+fung <- fung[fung_good_samples,fung_good_taxa]
+
+# indentify metadata rows for fungeria
+fung_meta <- 
+  meta %>% 
+  dplyr::filter(rawDataFileName %in% row.names(fung)) %>% 
+  dplyr::select(plotID,year,m_y,dnaSampleID,
+                siteID,collectDate,forwardPrimer,
+                reversePrimer,sequencerRunID,
+                rawDataFileName)
+
+# build metadata for physeq
+fung_samdata <- sample_data(fung_meta)
+sample_names(fung_samdata) <- fung_samdata$rawDataFileName
+
+# build physeq object
+fung_ps <- phyloseq(fung_samdata,fung)
+fung_ps@sam_data$rawDataFileName <- NULL
+
 # save to output
+saveRDS(fung_ps,"./output/fungal_physeq_no_taxonomy.RDS")
 saveRDS(fung,"./output/fungal_asv_table_combined.RDS")
+
 # clean up
 rm("fung")
 gc()
+
